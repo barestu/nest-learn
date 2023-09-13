0037c70932355177e0917566904c07fb129b0676
Fandy Barestu <fandy@softwareseni.com>
2023-09-13 12:54:09 +0700

    setup typeorm module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: 'secret123',
+      database: 'nestjs_development',
+      entities: [],
+      synchronize: true, // TODO: Turn off in production
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
